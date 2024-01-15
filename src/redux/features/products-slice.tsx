9e77491b4e28ad7c2c7c161ee6ddddf5e059a2ef
ArtemPaskall <artempaskal@gmail.com>
2024-01-15 17:13:44 +0200

    modify redux product slice

--- src/redux/features/products-slice.tsx
+++ src/redux/features/products-slice.tsx
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { Product } from '@/../types'
+import { Product } from '../../../types'
 
 interface ProductsState {
   products: Product[]

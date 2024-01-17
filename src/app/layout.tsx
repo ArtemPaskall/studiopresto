dbee578e3a454a3c82d66b77c82d32f88c704603
ArtemPaskall <artempaskal@gmail.com>
2024-01-17 13:46:32 +0200

    final commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/Vercel_favicon.svg" />
+      </head>
       <body className={roboto.className}>
         <ReduxProvider>
           <Header />
